01da0ae87bdb0731e367e743820fc110f346306d
Daniel Liu <danliu277@gmail.com>
2019-12-03 18:32:07 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -52,11 +52,11 @@ def naming_system
           {
             label: "Family",
             sub_category: {
-              label: "Genus",
-              {
-                label: "Species",
-                sub_category: nil
-              }
+              # label: "Genus",
+              # {
+              #   label: "Species",
+              #   sub_category: nil
+              # }
             }
           }
         }
